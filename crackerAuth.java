7f3c447abddebe5e24dbc4a010c7bf33d2523948
Thiago Cardoso <42587622+cardosothiago@users.noreply.github.com>
2020-10-20 12:08:40 -0300

    Update crackerAuth.java

--- crackerAuth.java
+++ crackerAuth.java
@@ -12,26 +12,26 @@ public class crackerAuth{
 	
 		StringBuilder password = new StringBuilder();
 		Process discoverPassword;
-		char[] c = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+		char[] character = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
 			     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 			     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
 			     'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
 			     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 		
 		loop:
-		for (int i1 = 0; i1 < c.length;i1++){
-			password.append(c[i1]);
-			for (int i2 = 0; i2 < c.length;i2++){
-				password.append(c[i2]);
-				for (int i3 = 0; i3 < c.length;i3++){
-					password.append(c[i3]);
-					for (int i4 = 0; i4 < c.length;i4++){
-						password.append(c[i4]);
-						for (int i5 = 0; i5 < c.length;i5++){
-							password.append(c[i5]);
-							for (int i6 = 0; i6 < c.length;i6++){
-								password.append(c[i6]);
-								Process executeProcess = Runtime.getRuntime().exec("C:/Users/cardo/Downloads/autenticador.exe administrador "+password); 
+		for (int position1 = 0; position1 < character.length;position1++){
+			password.append(character[position1]);
+			for (int position2 = 0; position2 < character.length;position2++){
+				password.append(character[position2]);
+				for (int position3 = 0; position3 < character.length;position3++){
+					password.append(character[position3]);
+					for (int position4 = 0; position4 < character.length;position4++){
+						password.append(character[position4]);
+						for (int position5 = 0; position5 < character.length;position5++){
+							password.append(character[position5]);
+							for (int position6 = 0; position6 < character.length;position6++){
+								password.append(character[position6]);
+								Process executeProcess = Runtime.getRuntime().exec("C:/Users/cardo/Downloads/autenticador.exe administrador " + password); 
 								int callBack = executeProcess.waitFor();
 								if(callBack == 0) {
 									System.out.println("Acesso concedido");
